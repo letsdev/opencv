62934dfcf8808ea40e7f2787b62d352f86b4a63a
Fabian Köbel <fabian.koebel@letsdev.de>
2020-06-04 15:33:46 +0200

    enabled x86_64 build.

--- platforms/android_build.sh
+++ platforms/android_build.sh
@@ -43,7 +43,6 @@ mv $INSTALL_DIR_NATIVE/libs/$ARCH/* $FINAL_RELEASE_FOLDER/libs/$ARCH
 mkdir -p $FINAL_RELEASE_FOLDER/3rdparty/libs/$ARCH
 mv $INSTALL_DIR_NATIVE/3rdparty/libs/$ARCH/* $FINAL_RELEASE_FOLDER/3rdparty/libs/$ARCH
 
-
 ################################################################################
 # armeabi-v7a build
 ################################################################################
@@ -64,7 +63,6 @@ mkdir -p $FINAL_RELEASE_FOLDER/libs/$ARCH
 mv $INSTALL_DIR_NATIVE/libs/$ARCH/* $FINAL_RELEASE_FOLDER/libs/$ARCH
 mkdir -p $FINAL_RELEASE_FOLDER/3rdparty/libs/$ARCH
 mv $INSTALL_DIR_NATIVE/3rdparty/libs/$ARCH/* $FINAL_RELEASE_FOLDER/3rdparty/libs/$ARCH
-
 
 ################################################################################
 # x86 build
@@ -91,23 +89,23 @@ mv $INSTALL_DIR_NATIVE/3rdparty/libs/$ARCH/* $FINAL_RELEASE_FOLDER/3rdparty/libs
 ################################################################################
 # x86_64 build
 ################################################################################
-#echo "building for x86_64"
-#
-#ARCH="x86_64"
-#rm -rf $ARM_BUILD_FOLDER
-#
-#scripts/cmake_android_arm.sh -DANDROID_STL=c++_shared -DBUILD_SHARED_LIBS=ON -DENABLE_LTO=ON -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_ZLIB=ON -DWITH_JAVA=ON -DANDROID_NATIVE_API_LEVEL=21 -DANDROID_ABI=$ARCH -DANDROID_TOOLCHAIN_NAME=x86_64-4.9
-#
-#cd $ARM_BUILD_FOLDER
-#make clean
-#make -j8
-#make install
-#cd ..
-#
-#mkdir -p $FINAL_RELEASE_FOLDER/libs/$ARCH
-#mv $INSTALL_DIR_NATIVE/libs/$ARCH/* $FINAL_RELEASE_FOLDER/libs/$ARCH
-#mkdir -p $FINAL_RELEASE_FOLDER/3rdparty/libs/$ARCH
-#mv $INSTALL_DIR_NATIVE/3rdparty/libs/$ARCH/* $FINAL_RELEASE_FOLDER/3rdparty/libs/$ARCH
+echo "building for x86_64"
+
+ARCH="x86_64"
+rm -rf $ARM_BUILD_FOLDER
+
+scripts/cmake_android_arm.sh -DWITH_IPP=OFF -DANDROID_STL=c++_shared -DBUILD_SHARED_LIBS=ON -DENABLE_LTO=ON -DBUILD_opencv_calib3d=OFF -DBUILD_opencv_dnn=OFF -DBUILD_opencv_features2d=OFF -DBUILD_opencv_flann=OFF -DBUILD_opencv_highgui=OFF -DBUILD_opencv_ml=OFF -DBUILD_opencv_objdetect=OFF -DBUILD_opencv_photo=OFF -DBUILD_opencv_stitching=OFF -DBUILD_opencv_video=OFF -DBUILD_opencv_videoio=OFF -DBUILD_opencv_videostab=OFF -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_ZLIB=ON -DWITH_JAVA=ON -DANDROID_NATIVE_API_LEVEL=21 -DANDROID_ABI=$ARCH -DANDROID_TOOLCHAIN_NAME=x86_64-4.9 -DBUILD_EXAMPLES=OFF -DBUILD_ANDROID_EXAMPLES=OFF  -DBUILD_DOCS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF
+
+cd $ARM_BUILD_FOLDER
+make clean
+make -j8
+make install
+cd ..
+
+mkdir -p $FINAL_RELEASE_FOLDER/libs/$ARCH
+mv $INSTALL_DIR_NATIVE/libs/$ARCH/* $FINAL_RELEASE_FOLDER/libs/$ARCH
+mkdir -p $FINAL_RELEASE_FOLDER/3rdparty/libs/$ARCH
+mv $INSTALL_DIR_NATIVE/3rdparty/libs/$ARCH/* $FINAL_RELEASE_FOLDER/3rdparty/libs/$ARCH
 
 ################################################################################
 # finish
--- platforms/deployNexusWindows.sh
+++ platforms/deployNexusWindows.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mvn deploy:deploy-file -DgroupId=org.opencv.android.framework \
+mvn deploy:deploy-file -DgroupId=org.opencv.windows.framework \
     -DartifactId=opencv-image-processing \
     -Dversion=3.3.0 \
     -Dpackaging=zip \
-    -Dfile=opencv.zip \
+    -Dfile=opencv_windows.zip \
     -DgeneratePom=true \
     -DrepositoryId=releases-extern \
-    -Durl=http://dev2/nexus/repository/ld-third-party-releases
+    -Durl=http://dev2/nexus/repository/ld-third-party-releases \
